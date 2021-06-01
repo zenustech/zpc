64b72b13ba3727d2b03b3976c9b0d51901f28b1a
littlemine <wxlwxl1993@zju.edu.cn>
2021-06-02 04:09:35 +0800

    general atomic operations (inc atomic_ref)

---
+++ include/zensim/execution/Atomics.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <atomic>
+
+#include "zensim/execution/ExecutionPolicy.hpp"
+
+namespace zs {
+
+  template <typename ExecTag, typename T>
+  constexpr T atomic_add(ExecTag, T* dest, const T val) noexcept {
+    if constexpr (ZS_ENABLE_CUDA && is_same_v<ExecTag, cuda_exec_tag>) {
+#if ZS_ENABLE_CUDA
+      return atomicAdd(dest, val);
+#endif
+    } else if constexpr (is_same_v<ExecTag, host_exec_tag>) {
+      const T old = *dest;
+      dest += val;
+      return old;
+    } else if constexpr (is_same_v<ExecTag, omp_exec_tag>) {
+#if 0
+      /// introduced in c++20
+      std::atomic_ref<T> target{const_cast<T&>(*dest)};
+      return target.fetch_add(val);
+#else
+#endif
+    }
+    throw std::runtime_error(
+        fmt::format("atomic_add(tag {}, ...) not viable\n", get_execution_space_tag(ExecTag{})));
+    return (T)0;
+  }
+
+}  // namespace zs

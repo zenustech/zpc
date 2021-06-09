833fddaead646b918a79f3465eefe449e6fd2745
littlemine <wxlwxl1993@zju.edu.cn>
2021-06-09 22:47:24 +0800

    minor inc header fix

--- include/zensim/math/bit/Bits.h
+++ include/zensim/math/bit/Bits.h
@@ -4,76 +4,94 @@
 #include <bitset>
 #include <limits>
 
+#include "zensim/TypeAlias.hpp"
+
 namespace zs {
 
   /// bitwise identical reinterpretation
   constexpr float int_as_float(int i) noexcept {
     static_assert(sizeof(int) == sizeof(float), "int bits != float bits");
-    union {int i{0}; float f;} u{};
+    union {
+      int i{0};
+      float f;
+    } u{};
     u.i = i;
     return u.f;
   }
   constexpr int float_as_int(float f) noexcept {
     static_assert(sizeof(int) == sizeof(float), "int bits != float bits");
-    union {int i{0}; float f;} u{};
+    union {
+      int i{0};
+      float f;
+    } u{};
     u.f = f;
     return u.i;
   }
   constexpr double longlong_as_double(long long l) noexcept {
     static_assert(sizeof(long long) == sizeof(double), "long long bits != double bits");
-    union {long long l{0}; double d;} u{};
+    union {
+      long long l{0};
+      double d;
+    } u{};
     u.l = l;
     return u.d;
   }
   constexpr long long double_as_longlong(double d) noexcept {
     static_assert(sizeof(long long) == sizeof(double), "long long bits != double bits");
-    union {long long l{0}; double d;} u{};
+    union {
+      long long l{0};
+      double d;
+    } u{};
     u.d = d;
     return u.l;
   }
   /// morton code
-  constexpr u32 expand_bits_32(u32 v) noexcept { // expands lower 10-bits to 30 bits
-		v = (v * 0x00010001u) & 0xFF0000FFu;
-		v = (v * 0x00000101u) & 0x0F00F00Fu;
-		v = (v * 0x00000011u) & 0xC30C30C3u;
-		v = (v * 0x00000005u) & 0x49249249u;
-		return v;
-	}
+  constexpr u32 expand_bits_32(u32 v) noexcept {  // expands lower 10-bits to 30 bits
+    v = (v * 0x00010001u) & 0xFF0000FFu;
+    v = (v * 0x00000101u) & 0x0F00F00Fu;
+    v = (v * 0x00000011u) & 0xC30C30C3u;
+    v = (v * 0x00000005u) & 0x49249249u;
+    return v;
+  }
   constexpr u64 expand_bits_64(u32 v) noexcept {
-		// 0x1fffff, 0x1f00000000ffff, 0x1f0000ff0000ff, 0x100f00f00f00f00f, 0x10c30c30c30c30c3, 0x1249249249249249
-		u64 x = v & 0x1fffff; // retrieve 21-bits
-		x = (x | x << 32) & 0x1f00000000ffff;
-		x = (x | x << 16) & 0x1f0000ff0000ff;
-		x = (x | x << 8)  & 0x100f00f00f00f00f;
-		x = (x | x << 4)  & 0x10c30c30c30c30c3;
-		x = (x | x << 2)  & 0x1249249249249249;
-		return x;
-	}
+    // 0x1fffff, 0x1f00000000ffff, 0x1f0000ff0000ff, 0x100f00f00f00f00f, 0x10c30c30c30c30c3,
+    // 0x1249249249249249
+    u64 x = v & 0x1fffff;  // retrieve 21-bits
+    x = (x | x << 32) & 0x1f00000000ffff;
+    x = (x | x << 16) & 0x1f0000ff0000ff;
+    x = (x | x << 8) & 0x100f00f00f00f00f;
+    x = (x | x << 4) & 0x10c30c30c30c30c3;
+    x = (x | x << 2) & 0x1249249249249249;
+    return x;
+  }
   constexpr u32 compact_bits_32(u32 v) noexcept {
-		// 0, 0x000003ff, 0x30000ff, 0x0300f00f, 0x30c30c3, 0x9249249
-		v &= 0x9249249;
-		v = (v ^ (v >> 2)) & 0x30c30c3;
-		v = (v ^ (v >> 4)) & 0x0300f00f;
-		v = (v ^ (v >> 8)) & 0x30000ff;
-		v = (v ^ (v >> 16)) & 0x000003ff;
-		return v;
-	}
+    // 0, 0x000003ff, 0x30000ff, 0x0300f00f, 0x30c30c3, 0x9249249
+    v &= 0x9249249;
+    v = (v ^ (v >> 2)) & 0x30c30c3;
+    v = (v ^ (v >> 4)) & 0x0300f00f;
+    v = (v ^ (v >> 8)) & 0x30000ff;
+    v = (v ^ (v >> 16)) & 0x000003ff;
+    return v;
+  }
   constexpr u32 compact_bits_64(u64 v) noexcept {
-		// 0x1fffff, 0x1f00000000ffff, 0x1f0000ff0000ff, 0x100f00f00f00f00f, 0x10c30c30c30c30c3, 0x1249249249249249
-		v &= 0x1249249249249249;
-		v = (v ^ (v >> 2)) & 0x10c30c30c30c30c3;
-		v = (v ^ (v >> 4)) & 0x100f00f00f00f00f;
-		v = (v ^ (v >> 8)) & 0x1f0000ff0000ff;
-		v = (v ^ (v >> 16)) & 0x1f00000000ffff;
-		v = (v ^ (v >> 32)) & 0x1fffff;
-		return static_cast<u32>(v);
-	}
+    // 0x1fffff, 0x1f00000000ffff, 0x1f0000ff0000ff, 0x100f00f00f00f00f, 0x10c30c30c30c30c3,
+    // 0x1249249249249249
+    v &= 0x1249249249249249;
+    v = (v ^ (v >> 2)) & 0x10c30c30c30c30c3;
+    v = (v ^ (v >> 4)) & 0x100f00f00f00f00f;
+    v = (v ^ (v >> 8)) & 0x1f0000ff0000ff;
+    v = (v ^ (v >> 16)) & 0x1f00000000ffff;
+    v = (v ^ (v >> 32)) & 0x1fffff;
+    return static_cast<u32>(v);
+  }
   constexpr u32 morton_3d_32(float x, float y, float z) noexcept {
-		return (expand_bits_32((u32)(x * 1024.f)) << 2) | (expand_bits_32((u32)(y * 1024.f)) << 1) | expand_bits_32((u32)(z * 1024.f));
-	}
+    return (expand_bits_32((u32)(x * 1024.f)) << 2) | (expand_bits_32((u32)(y * 1024.f)) << 1)
+           | expand_bits_32((u32)(z * 1024.f));
+  }
   constexpr u64 morton_3d_64(double x, double y, double z) {
-		return (expand_bits_64((u32)(x * 2097152.)) << 2) | (expand_bits_64((u32)(y * 2097152.)) << 1) | expand_bits_64((u32)(z * 2097152.));
-	}
+    return (expand_bits_64((u32)(x * 2097152.)) << 2) | (expand_bits_64((u32)(y * 2097152.)) << 1)
+           | expand_bits_64((u32)(z * 2097152.));
+  }
   /**
    */
   template <typename Integer> constexpr Integer interleaved_bit_mask(int dim) noexcept {

4990c58b2afe94862e95052d2ac00ce82915fc01
littlemine <wxlwxl1993@zju.edu.cn>
2021-08-23 07:03:49 +0800

    approximate (r)sqrt

--- include/zensim/math/MathUtils.h
+++ include/zensim/math/MathUtils.h
@@ -31,6 +31,48 @@ namespace zs {
     }
   }  // namespace mathutil_impl
 
+  namespace math {
+    /// ref: http://www.lomont.org/papers/2003/InvSqrt.pdf
+    /// ref: https://cs.uwaterloo.ca/~m32rober/rsqrt.pdf
+    /// ref: https://community.wolfram.com/groups/-/m/t/1108896
+    /// ref:
+    /// https://www.codeproject.com/Articles/69941/Best-Square-Root-Method-Algorithm-Function-Precisi
+    constexpr float q_rsqrt(float number) noexcept {
+      uint32_t i{};
+      float x2 = number * 0.5f, y = number;
+      i = *(uint32_t *)&y;
+      i = 0x5f375a86 - (i >> 1);
+      y = *(float *)&i;
+      y = y * (1.5f - (x2 * y * y));
+      y = y * (1.5f - (x2 * y * y));
+      return y;
+    }
+    constexpr double q_rsqrt(double number) noexcept {
+      uint64_t i{};
+      double x2 = number * 0.5, y = number;
+      i = *(uint64_t *)&y;
+      i = 0x5fe6eb50c7b537a9 - (i >> 1);
+      y = *(double *)&i;
+      y = y * (1.5 - (x2 * y * y));
+      y = y * (1.5 - (x2 * y * y));
+      return y;
+    }
+    constexpr float q_sqrt(float x) noexcept {
+#if 1
+      return 1.f / q_rsqrt(x);
+#else
+      uint32_t i = *(uint32_t *)&x;
+      // adjust bias
+      i += 127 << 23;
+      // approximation of square root
+      i >>= 1;
+      return *(float *)&i;
+#endif
+    }
+    // best guess starting square
+    constexpr double q_sqrt(double fp) noexcept { return 1.0 / q_rsqrt(fp); }
+  }  // namespace math
+
   template <typename... Args>
   constexpr auto incl_prefix_sum(std::size_t I, Args &&...args) noexcept {
     return mathutil_impl::incl_prefix_sum_impl(I, std::index_sequence_for<Args...>{},

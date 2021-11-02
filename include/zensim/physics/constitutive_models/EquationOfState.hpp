9f40e7916da4ad2e542197b73c5fdf11cbb9d327
littlemine <wxlwxl1993@zju.edu.cn>
2021-11-03 03:00:53 +0800

    update file lists & add eos

---
+++ include/zensim/physics/constitutive_models/EquationOfState.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include "../ConstitutiveModel.hpp"
+
+namespace zs {
+
+  template <typename T = float> struct EquationOfState
+      : IsotropicConstitutiveModelInterface<EquationOfState<T>> {
+    using base_t = IsotropicConstitutiveModelInterface<EquationOfState<T>>;
+    using value_type = T;
+
+    static_assert(std::is_floating_point_v<value_type>, "value type should be floating point");
+
+    value_type bulk, gamma;  // gamma is 7
+
+    constexpr EquationOfState(value_type bulk, value_type gamma = 7) noexcept
+        : bulk{bulk}, gamma{7} {}
+
+    // psi
+    template <typename V> constexpr V psi(V J) const noexcept {
+      const auto J2 = J * J;
+      const auto J6 = J2 * J2 * J2;
+      // return -bulk * ((T)1 / (T)(-6) / J6 - J);
+      return bulk * ((V)1 / (V)6 / J6 + J);
+    }
+    // first_piola
+    template <typename V> constexpr V first_piola(V J) const noexcept {
+      const auto J2 = J * J;
+      const auto J4 = J2 * J2;
+      const auto J7 = J4 * J2 * J;
+      // return -bulk * ((T)1 / J7 - (T)1);
+      return bulk - bulk / J7;
+    }
+    // first_piola_derivative
+    template <typename V> constexpr V first_piola_derivative(V J) const noexcept {
+      const auto J2 = J * J;
+      const auto J4 = J2 * J2;
+      const auto J8 = J4 * J4;
+      // return bulk * ((T)1 / J8 * (T)7);
+      return bulk * (V)7 / J8;
+    }
+  };
+
+}  // namespace zs

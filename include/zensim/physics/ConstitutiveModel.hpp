6040a35c97fa1197c745ca9066a2c74ce98e1ebd
littlemine <wxlwxl1993@zju.edu.cn>
2021-11-02 20:39:41 +0800

    add isotropic constitutive model interface

--- include/zensim/physics/ConstitutiveModel.hpp
+++ include/zensim/physics/ConstitutiveModel.hpp
@@ -24,6 +24,183 @@ namespace zs {
     return std::make_tuple(mu, lam);
   }
 
+  template <typename Model> struct IsotropicConstitutiveModelInterface {
+#define DECLARE_ISOTROPIC_CONSTITUTIVE_MODEL_INTERFACE_ATTRIBUTES \
+  using value_type = typename Model::value_type;
+
+    using model_type = Model;
+    template <typename VecT> using vec_type = typename VecT::template variant_vec<
+        typename VecT::value_type,
+        integer_seq<typename VecT::index_type, VecT::template range<0>()>>;
+    template <typename VecT> using mat_type = typename VecT::template variant_vec<
+        typename VecT::value_type, integer_seq<typename VecT::index_type, VecT::template range<0>(),
+                                               VecT::template range<0>()>>;
+    template <typename VecT> using Bij_mat_type =
+        typename VecT::template variant_vec<typename VecT::value_type,
+                                            integer_seq<typename VecT::index_type, 2, 2>>;
+
+    // psi_sigma
+    template <typename VecT, enable_if_all<VecT::dim == 1, (VecT::template range<0>() <= 3),
+                                           std::is_floating_point_v<typename VecT::value_type>> = 0>
+    constexpr auto psi_sigma(const VecInterface<VecT>& S) const noexcept {
+      return static_cast<const Model*>(this)->do_psi_sigma(S);
+    }
+    // dpsi_dsigma
+    template <typename VecT, enable_if_all<VecT::dim == 1, (VecT::template range<0>() <= 3),
+                                           std::is_floating_point_v<typename VecT::value_type>> = 0>
+    constexpr auto dpsi_dsigma(const VecInterface<VecT>& S) const noexcept {
+      return static_cast<const Model*>(this)->do_dpsi_dsigma(S);
+    }
+    // d2psi_dsigma2
+    template <typename VecT, enable_if_all<VecT::dim == 1, (VecT::template range<0>() <= 3),
+                                           std::is_floating_point_v<typename VecT::value_type>> = 0>
+    constexpr auto d2psi_dsigma2(const VecInterface<VecT>& S) const noexcept {
+      return static_cast<const Model*>(this)->do_d2psi_dsigma2(S);
+    }
+    // Bij_neg_coeff
+    template <typename VecT,
+              enable_if_all<VecT::dim == 1,
+                            (VecT::template range<0>() == 2 || VecT::template range<0>() == 3),
+                            std::is_floating_point_v<typename VecT::value_type>> = 0>
+    constexpr auto Bij_neg_coeff(const VecInterface<VecT>& S) const noexcept {
+      return static_cast<const Model*>(this)->do_Bij_neg_coeff(S);
+    }
+
+    // details (default impls)
+    template <typename VecT>
+    constexpr typename VecT::value_type do_psi_sigma(const VecInterface<VecT>& S) const noexcept {
+      return (typename VecT::value_type)0;
+    }
+    template <typename VecT>
+    constexpr auto do_dpsi_dsigma(const VecInterface<VecT>& S) const noexcept {
+      return vec_type<VecT>::zeros();
+    }
+    template <typename VecT>
+    constexpr auto do_d2psi_dsigma2(const VecInterface<VecT>& S) const noexcept {
+      return mat_type<VecT>::zeros();
+    }
+    template <typename VecT>
+    constexpr auto do_Bij_neg_coeff(const VecInterface<VecT>& S) const noexcept {
+      using RetT = typename VecT::template variant_vec<
+          typename VecT::value_type,
+          integer_seq<typename VecT::index_type, (VecT::template range<0>() == 3 ? 3 : 1)>>;
+      return RetT::zeros();
+    }
+
+    // psi
+    template <typename VecT, enable_if_all<VecT::dim == 2, (VecT::template range<0>() <= 3),
+                                           VecT::template range<0>() == VecT::template range<1>(),
+                                           std::is_floating_point_v<typename VecT::value_type>> = 0>
+    constexpr auto psi(const VecInterface<VecT>& F) const noexcept {
+      auto [U, S, V] = math::svd(F);
+      return static_cast<const Model*>(this)->psi_sigma(S);
+    }
+    // first piola
+    template <typename VecT, enable_if_all<VecT::dim == 2, (VecT::template range<0>() <= 3),
+                                           VecT::template range<0>() == VecT::template range<1>(),
+                                           std::is_floating_point_v<typename VecT::value_type>> = 0>
+    constexpr auto first_piola(const VecInterface<VecT>& F) const noexcept {
+      auto [U, S, V] = math::svd(F);
+      auto dE_dsigma = static_cast<const Model*>(this)->dpsi_dsigma(S);
+      return diag_mul(U, dE_dsigma) * V.transpose();
+    }
+    // first piola derivative
+    template <typename VecT, enable_if_all<VecT::dim == 2, (VecT::template range<0>() <= 3),
+                                           VecT::template range<0>() == VecT::template range<1>(),
+                                           std::is_floating_point_v<typename VecT::value_type>> = 0>
+    constexpr auto first_piola_derivative(const VecInterface<VecT>& F) const noexcept {
+      using T = typename VecT::value_type;
+      using Ti = typename VecT::index_type;
+      using extents = typename VecT::extents;
+      constexpr int dim = VecT::template range<0>();
+
+      auto [U, S, V] = math::svd(F);
+      auto dE_dsigma = static_cast<const Model*>(this)->dpsi_dsigma(S);
+      // A
+      auto d2E_dsigma2 = static_cast<const Model*>(this)->d2psi_dsigma2(S);
+      // Bij
+      using MatB = typename VecT::template variant_vec<T, integer_seq<Ti, 2, 2>>;
+      auto ComputeBij = [&dE_dsigma, &S = S,
+                         Bij_left_coeffs = Bij_neg_coeff(S)](int i) -> MatB {  // i -> i, i + 1
+        int j = (i + 1) % dim;
+        T leftCoeff = Bij_left_coeffs[i];
+        T rightDenom = gcem::max(S[i] + S[j], (T)1e-6);  // prevents division instability
+        T rightCoeff = (dE_dsigma[i] + dE_dsigma[j]) / (rightDenom + rightDenom);
+        return MatB{leftCoeff + rightCoeff, leftCoeff - rightCoeff, leftCoeff - rightCoeff,
+                    leftCoeff + rightCoeff};
+      };
+      using MatH = typename VecT::template variant_vec<T, integer_seq<Ti, dim * dim, dim * dim>>;
+      auto M = MatH::zeros();
+      MatH dPdF{};
+
+      if constexpr (is_same_v<typename VecT::dims, sindex_seq<3, 3>>) {
+        auto B0 = ComputeBij(0) /*B12*/, B1 = ComputeBij(1) /*B23*/, B2 = ComputeBij(2) /*B13*/;
+        fmt::print("B0: [{}, {}], [{}, {}]\n", B0(0, 0), B0(0, 1), B0(1, 0), B0(1, 1));
+        fmt::print("B1: [{}, {}], [{}, {}]\n", B1(0, 0), B1(0, 1), B1(1, 0), B1(1, 1));
+        fmt::print("B2: [{}, {}], [{}, {}]\n", B2(0, 0), B2(0, 1), B2(1, 0), B2(1, 1));
+        // [A_00, 0,      0] [0,      A_01, 0     ] [0,       0,      A_02]
+        // [0,    B12_00, 0] [B12_01, 0,    0     ] [0,       0,      0   ]
+        // [0,    0, B13_11] [0,      0,    0     ] [B13_10,  0,      0   ]
+        // [0,    B12_10, 0] [B12_11, 0,    0     ] [0,       0,      0   ]
+        // [A_10, 0,      0] [0,      A_11, 0     ] [0,       0,      A_12]
+        // [0,    0,      0] [0,      0,    B23_00] [0,       B23_01, 0   ]
+        // [0,    0, B13_01] [0,      0,    0     ] [B13_00,  0,      0   ]
+        // [0,    0,      0] [0,      0,    B23_10] [0,       B23_11, 0   ]
+        // [A_20, 0,      0] [0,      A_21, 0     ] [0,       0,      A_22]
+        for (int ji = 0; ji != dim * dim; ++ji) {
+          int j = ji / dim;
+          int i = ji - j * dim;
+          for (int sr = 0; sr <= ji; ++sr) {
+            int s = sr / dim;
+            int r = sr - s * dim;
+            dPdF(ji, sr) = dPdF(sr, ji)
+                = d2E_dsigma2(0, 0) * U(i, 0) * V(j, 0) * U(r, 0) * V(s, 0)
+                  + d2E_dsigma2(0, 1) * U(i, 0) * V(j, 0) * U(r, 1) * V(s, 1)
+                  + d2E_dsigma2(0, 2) * U(i, 0) * V(j, 0) * U(r, 2) * V(s, 2)
+                  + d2E_dsigma2(1, 0) * U(i, 1) * V(j, 1) * U(r, 0) * V(s, 0)
+                  + d2E_dsigma2(1, 1) * U(i, 1) * V(j, 1) * U(r, 1) * V(s, 1)
+                  + d2E_dsigma2(1, 2) * U(i, 1) * V(j, 1) * U(r, 2) * V(s, 2)
+                  + d2E_dsigma2(2, 0) * U(i, 2) * V(j, 2) * U(r, 0) * V(s, 0)
+                  + d2E_dsigma2(2, 1) * U(i, 2) * V(j, 2) * U(r, 1) * V(s, 1)
+                  + d2E_dsigma2(2, 2) * U(i, 2) * V(j, 2) * U(r, 2) * V(s, 2)
+                  + B0(0, 0) * U(i, 0) * V(j, 1) * U(r, 0) * V(s, 1)
+                  + B0(0, 1) * U(i, 0) * V(j, 1) * U(r, 1) * V(s, 0)
+                  + B0(1, 0) * U(i, 1) * V(j, 0) * U(r, 0) * V(s, 1)
+                  + B0(1, 1) * U(i, 1) * V(j, 0) * U(r, 1) * V(s, 0)
+                  + B1(0, 0) * U(i, 1) * V(j, 2) * U(r, 1) * V(s, 2)
+                  + B1(0, 1) * U(i, 1) * V(j, 2) * U(r, 2) * V(s, 1)
+                  + B1(1, 0) * U(i, 2) * V(j, 1) * U(r, 1) * V(s, 2)
+                  + B1(1, 1) * U(i, 2) * V(j, 1) * U(r, 2) * V(s, 1)
+                  + B2(1, 1) * U(i, 0) * V(j, 2) * U(r, 0) * V(s, 2)
+                  + B2(1, 0) * U(i, 0) * V(j, 2) * U(r, 2) * V(s, 0)
+                  + B2(0, 1) * U(i, 2) * V(j, 0) * U(r, 0) * V(s, 2)
+                  + B2(0, 0) * U(i, 2) * V(j, 0) * U(r, 2) * V(s, 0);
+          }
+        }
+      } else if constexpr (is_same_v<typename VecT::dims, sindex_seq<2, 2>>) {
+        auto B = ComputeBij(0);
+        for (int ji = 0; ji != dim * dim; ++ji) {
+          int j = ji / dim;
+          int i = ji - j * dim;
+          for (int sr = 0; sr <= ji; ++sr) {
+            int s = sr / dim;
+            int r = sr - s * dim;
+            dPdF(ji, sr) = dPdF(sr, ji)
+                = d2E_dsigma2(0, 0) * U(i, 0) * V(j, 0) * U(r, 0) * V(s, 0)
+                  + d2E_dsigma2(0, 1) * U(i, 0) * V(j, 0) * U(r, 1) * V(s, 1)
+                  + B(0, 0) * U(i, 0) * V(j, 1) * U(r, 0) * V(s, 1)
+                  + B(0, 1) * U(i, 0) * V(j, 1) * U(r, 1) * V(s, 0)
+                  + B(1, 0) * U(i, 1) * V(j, 0) * U(r, 0) * V(s, 1)
+                  + B(1, 1) * U(i, 1) * V(j, 0) * U(r, 1) * V(s, 0)
+                  + d2E_dsigma2(1, 0) * U(i, 1) * V(j, 1) * U(r, 0) * V(s, 0)
+                  + d2E_dsigma2(1, 1) * U(i, 1) * V(j, 1) * U(r, 1) * V(s, 1);
+          }
+        }
+      }
+      return dPdF;
+    }
+  };
+
   struct MaterialConfig {
     float rho{1e3};
     float volume{1};
@@ -88,39 +265,39 @@ namespace zs {
       = variant<EquationOfStateConfig, NeoHookeanConfig, FixedCorotatedConfig,
                 VonMisesFixedCorotatedConfig, DruckerPragerConfig, NACCConfig>;
 
-  constexpr bool particleHasF(const ConstitutiveModelConfig &model) noexcept {
+  constexpr bool particleHasF(const ConstitutiveModelConfig& model) noexcept {
     return model.index() != 0;
   }
-  constexpr bool particleHasJ(const ConstitutiveModelConfig &model) noexcept {
+  constexpr bool particleHasJ(const ConstitutiveModelConfig& model) noexcept {
     return model.index() == 0;
   }
 
-  inline void displayConfig(ConstitutiveModelConfig &config) {
+  inline void displayConfig(ConstitutiveModelConfig& config) {
     match(
-        [](EquationOfStateConfig &config) {
+        [](EquationOfStateConfig& config) {
           fmt::print("rho {}, volume {}, dim {}\n", config.rho, config.volume, config.dim);
           fmt::print("bulk {}, gamma {}, viscosity{}\n", config.bulk, config.gamma,
                      config.viscosity);
         },
-        [](NeoHookeanConfig &config) {
+        [](NeoHookeanConfig& config) {
           fmt::print("rho {}, volume {}, dim {}\n", config.rho, config.volume, config.dim);
           fmt::print("E {}, nu {}\n", config.E, config.nu);
         },
-        [](FixedCorotatedConfig &config) {
+        [](FixedCorotatedConfig& config) {
           fmt::print("rho {}, volume {}, dim {}\n", config.rho, config.volume, config.dim);
           fmt::print("E {}, nu {}\n", config.E, config.nu);
         },
-        [](VonMisesFixedCorotatedConfig &config) {
+        [](VonMisesFixedCorotatedConfig& config) {
           fmt::print("rho {}, volume {}, dim {}\n", config.rho, config.volume, config.dim);
           fmt::print("E {}, nu {}, yieldStress {}\n", config.E, config.nu, config.yieldStress);
         },
-        [](DruckerPragerConfig &config) {
+        [](DruckerPragerConfig& config) {
           fmt::print("rho {}, volume {}, dim {}\n", config.rho, config.volume, config.dim);
           fmt::print("E {}, nu {}, logJp0 {}, fric_angle {}, cohesion {}, beta, yieldSurface {}\n",
                      config.E, config.nu, config.logJp0, config.fa, config.cohesion, config.beta,
                      config.yieldSurface);
         },
-        [](NACCConfig &config) {
+        [](NACCConfig& config) {
           fmt::print("rho {}, volume {}, dim {}\n", config.rho, config.volume, config.dim);
           fmt::print("E {}, nu {}, logJp0 {}, fric_angle {}, xi {}, beta {}, mohrColumbFric {}\n",
                      config.E, config.nu, config.logJp0, config.fa, config.xi, config.beta,
@@ -140,7 +317,7 @@ namespace zs {
     return dw;
   }
   template <typename T, auto dim>
-  constexpr vec<T, dim, 3> bspline_weight(const vec<T, dim> &p, T const dx_inv) noexcept {
+  constexpr vec<T, dim, 3> bspline_weight(const vec<T, dim>& p, T const dx_inv) noexcept {
     vec<T, dim, 3> dw{};
     for (int i = 0; i < dim; ++i) {
       T d = p(i) * dx_inv - (lower_trunc(p(i) * dx_inv + 0.5) - 1);

111c98a559ec0e9f810dcd7d20bdd3f550050c3c
littlemine <wxlwxl1993@zju.edu.cn>
2021-11-10 23:44:28 +0800

    C (vel grad) -> strain rate

--- include/zensim/physics/plasticity_models/VonMisesCapped.hpp
+++ include/zensim/physics/plasticity_models/VonMisesCapped.hpp
@@ -58,6 +58,79 @@ namespace zs {
       do_project_sigma(S, model);
       F = diag_mul(U, S) * V.transpose();
     }
+
+    template <typename VecT, typename VecTV, typename Model,
+              enable_if_all<VecT::dim == 2, (VecT::template range<0>() <= 3),
+                            VecT::template range<0>() == VecT::template range<1>(),
+                            std::is_floating_point_v<typename VecT::value_type>> = 0>
+    constexpr void project_strain(VecInterface<VecT>& F, const Model& model,
+                                  const VecInterface<VecTV>& Chat, int pi = -1) const noexcept {
+      auto [U, S, V] = math::svd(F);
+
+      using value_type = typename VecT::value_type;
+      using Ti = typename VecT::index_type;
+      using extents = typename VecT::extents;
+      constexpr int dim = VecT::template range<0>();
+
+      const auto _2mu = (static_cast<const Model&>(model).mu + static_cast<const Model&>(model).mu);
+      const auto dim_mul_lam = (value_type)dim * (value_type) static_cast<const Model&>(model).lam;
+
+      auto eps = S.log();
+      auto eps_trace = eps.sum();
+      auto dev_eps = eps - eps_trace / (value_type)dim;  // equivalent to eps.deviatoric()
+
+#if 0
+      if (pi < 0)
+        printf("pi[%d]: dev(eps) = %f, %f, %f; eps = %f, %f, %f; trace %f; normSqr %f; norm %f\n",
+               pi, (float)dev_eps(0), (float)dev_eps(1), (float)dev_eps(2), (float)eps(0),
+               (float)eps(1), (float)eps(2), (float)eps_trace, (float)dev_eps.l2NormSqr(),
+               math::sqrtNewtonRaphson((float)dev_eps.l2NormSqr()));
+#endif
+
+      auto dev_eps_norm = dev_eps.norm();
+
+      // Cowper-Symonds
+      auto P = (value_type)10;
+      auto C = (value_type)0.1;
+      auto coef = gcem::pow((Chat.sum() / (value_type)dim) / C, (value_type)1 / P);
+#if 1
+      auto ys
+          = yieldStress * (1 + gcem::pow((Chat.sum() / (value_type)dim) / C, (value_type)1 / P));
+#else
+      auto ys = yieldStress;
+#endif
+
+      auto delta_gamma = dev_eps_norm - ys / _2mu;
+// printf("%f -> %f\n", yieldStress, ys);
+#if 0
+      if (dev_eps_norm > 0.2f && pi < 10)
+        printf(
+            "pi[%d]: sigma_0: %f -> sigma_yield: %f (coeff: %f), mu: %f, candidate: %f, right: "
+            "%f\n",
+            pi, (float)yieldStress, (float)ys, (float)coef, (float)(_2mu * 0.5),
+            (float)dev_eps_norm, (float)(ys / _2mu));
+#endif
+
+      if (delta_gamma > 0) {
+        auto H = eps - (delta_gamma / dev_eps_norm) * dev_eps;
+        S = H.exp();
+#if 0
+        if (dev_eps_norm > 0.2f)
+          printf("H: %f, %f, %f; S %f, %f, %f -> %f, %f, %f\n", (float)H[0], (float)H[1],
+                 (float)H[2], (float)oldS[0], (float)oldS[1], (float)oldS[2], (float)S[0],
+                 (float)S[1], (float)S[2]);
+#endif
+      }
+
+#if 0
+      if (eps_trace > k1Stretch / (dim_mul_lam + _2mu))
+        S = S * gcem::exp(k1Stretch / (dim * dim_mul_lam + dim * _2mu) - eps_trace / (value_type)dim);
+      else if (eps_trace < -k1Compress / (dim_mul_lam + _2mu))
+        S = S * gcem::exp(-k1Compress / (dim * dim_mul_lam + dim * _2mu) - eps_trace / (value_type)dim);
+#endif
+
+      F = diag_mul(U, S) * V.transpose();
+    }
   };
 
 }  // namespace zs

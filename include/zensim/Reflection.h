ff0acaac439b58970cb21a6d269e2930562f80b0
littlemine <wxlwxl1993@zju.edu.cn>
2021-12-02 23:38:07 +0800

    enhance reflection

--- include/zensim/Reflection.h
+++ include/zensim/Reflection.h
@@ -59,4 +59,89 @@ namespace zs {
     that_type<decltype(param)> paramType;
   }
 
+  namespace detail {
+    template <typename T> constexpr auto get_var_type_str_helper(T &&) noexcept {
+#if defined(_MSC_VER)
+      return __FUNCSIG__;
+#else
+      return __PRETTY_FUNCTION__;
+#endif
+    }
+
+    template <typename T> constexpr auto get_type_str_helper() noexcept {
+#if defined(_MSC_VER)
+      return __FUNCSIG__;
+#else
+      return __PRETTY_FUNCTION__;
+#endif
+    }
+
+    constexpr std::size_t get_type_len_helper(const char *p = nullptr) noexcept {
+      if (p == nullptr) return (std::size_t)0;
+      std::size_t i = 0;
+      for (; p[i]; ++i)
+        ;
+      return i;
+    }
+
+    struct range_pair {
+      std::size_t l{}, r{};
+    };
+    constexpr range_pair locate_char_in_str_helper(const char *p, const char lc,
+                                                   const char rc) noexcept {
+      if (p == nullptr) return range_pair{0, 0};
+      std::size_t l{0};
+      for (; *p; ++p, ++l)
+        if (*p == lc) break;
+      std::size_t r{l + 1}, cnt{1};
+      for (; *p; ++p, ++r) {
+        if (*p == lc)
+          cnt++;
+        else if (*p == rc)
+          cnt--;
+        if (cnt == 0) break;
+      }
+      /// [l, r]
+      return range_pair{l, r};
+    }
+
+    template <std::size_t head = 0, std::size_t length = 0, typename T>
+    constexpr auto get_var_type_substr(T &&v) noexcept {
+      constexpr auto typestr = get_type_str_helper<T>();
+      using CharT = std::remove_const_t<std::remove_pointer_t<decltype(typestr)>>;
+      constexpr auto typelength = get_type_len_helper(typestr);
+      static_assert(typelength > head, "sub-string should not exceed the whole string!");
+      constexpr auto substrLength
+          = (length == 0 ? typelength - head
+                         : (length < (typelength - head) ? length : (typelength - head)));
+      std::array<CharT, substrLength> ret{};
+      for (std::size_t i = 0; i != substrLength; ++i) ret[i] = typestr[i + head];
+      return ret;
+    }
+  }  // namespace detail
+
+  template <typename T> constexpr auto get_var_type_substr(T &&v) noexcept {
+    constexpr auto typestr = detail::get_type_str_helper<T>();
+    using CharT = std::remove_const_t<std::remove_pointer_t<decltype(typestr)>>;
+    constexpr auto typelength = detail::get_type_len_helper(typestr);
+
+#if defined(_MSC_VER)
+    constexpr auto pair = detail::locate_char_in_str_helper(typestr, '<', '>');
+    constexpr std::size_t head{pair.l + 1};
+    constexpr std::size_t length{pair.r - 2 - head};
+#elif defined(__clang__)
+    constexpr auto pair = detail::locate_char_in_str_helper(typestr, '[', ']');
+    constexpr std::size_t head{pair.l + 5};
+    constexpr std::size_t length{pair.r - 2 - head};
+#elif defined(__GNUC__)
+    constexpr auto pair = detail::locate_char_in_str_helper(typestr, '[', ']');
+    constexpr std::size_t head{pair.l + 10};
+    constexpr std::size_t length{pair.r - 2 - head};
+#endif
+
+    std::array<CharT, length> ret{};
+    for (std::size_t i = 0; i != length; ++i) ret[i] = typestr[i + head];
+    return ret;
+  }
+
 }  // namespace zs

362b13a33fb0f8a341606b29a6194996e6183947
littlemine <wxlwxl1993@zju.edu.cn>
2022-03-01 01:34:13 +0800

    WIP: arena integration with levelset

--- include/zensim/geometry/LevelSetUtils.hpp
+++ include/zensim/geometry/LevelSetUtils.hpp
@@ -2,8 +2,149 @@
 #include "SparseLevelSet.hpp"
 #include "zensim/container/HashTable.hpp"
 #include "zensim/execution/ExecutionPolicy.hpp"
+#include "zensim/math/Vec.h"
+#include "zensim/math/curve/InterpolationKernel.hpp"
+#include "zensim/tpls/magic_enum/magic_enum.hpp"
 
 namespace zs {
+
+  template <typename LsvT, kernel_e kt_ = kernel_e::quadratic, int drv_order = 0>
+  struct LevelSetArena {
+    using lsv_t = LsvT;
+    using value_type = typename lsv_t::value_type;
+    using index_type = typename lsv_t::index_type;
+
+    static_assert(std::is_signed_v<index_type>, "index_type should be a signed integer.");
+    static constexpr grid_e category = lsv_t::category;
+    static constexpr kernel_e kt = kt_;
+    static constexpr int dim = lsv_t::dim;
+    static constexpr index_type width = magic_enum::enum_integer(kt);
+    static constexpr int deriv_order = drv_order;
+    static constexpr index_type coord_offset
+        = kt == kernel_e::quadratic ? -1 : (kt == kernel_e::linear ? 0 : -2);
+
+    using TV = typename lsv_t::TV;
+    using TWM = vec<value_type, dim, width>;
+    using IV = typename lsv_t::IV;
+
+    using coord_index_type = typename lsv_t::coord_index_type;
+    using channel_counter_type = typename lsv_t::channel_counter_type;
+    using cell_index_type = typename lsv_t::cell_index_type;
+
+    static_assert(deriv_order >= 0 && deriv_order <= 2,
+                  "weight derivative order should be a integer within [0, 2]");
+    using WeightScratchPad
+        = conditional_t<deriv_order == 0, tuple<TWM>,
+                        conditional_t<deriv_order == 1, tuple<TWM, TWM>, tuple<TWM, TWM, TWM>>>;
+    template <typename Val, std::size_t... Is>
+    static constexpr auto arena_type_impl(index_seq<Is...>) {
+      return vec<Val, (Is + 1 > 0 ? width : width)...>{};
+    }
+    template <typename Val, int d> static constexpr auto arena_type() {
+      return arena_type_impl<Val>(std::make_index_sequence<d>{});
+    }
+    template <typename Val> using Arena = decltype(arena_type<Val, dim>());
+
+    template <typename VecT, auto cate = category,
+              enable_if_all<VecT::dim == 1, VecT::extent == dim, cate != grid_e::staggered> = 0>
+    constexpr LevelSetArena(lsv_t &lsv, const VecInterface<VecT> &x, wrapv<kt> = {},
+                            wrapv<deriv_order> = {}) noexcept
+        : lsPtr{&lsv}, weights{}, iLocalPos{}, iCorner{} {
+      auto X = lsv.worldToIndex(x);
+      for (int d = 0; d != dim; ++d) iCorner[d] = lower_trunc(X[d]) + coord_offset;
+      iLocalPos = X - iCorner;
+      if constexpr (kt == kernel_e::linear)
+        weights = linear_bspline_weights<deriv_order>(iLocalPos);
+      else if constexpr (kt == kernel_e::quadratic)
+        weights = quadratic_bspline_weights<deriv_order>(iLocalPos);
+      else if constexpr (kt == kernel_e::cubic)
+        weights = cubic_bspline_weights<deriv_order>(iLocalPos);
+    }
+    template <typename VecT, auto cate = category,
+              enable_if_all<VecT::dim == 1, VecT::extent == dim, cate == grid_e::staggered> = 0>
+    constexpr LevelSetArena(lsv_t &lsv, const VecInterface<VecT> &x, int f, wrapv<kt> = {},
+                            wrapv<deriv_order> = {}) noexcept
+        : lsPtr{&lsv}, weights{}, iLocalPos{}, iCorner{} {
+      auto X = lsv.worldToIndex(x);
+      for (int d = 0; d != dim; ++d)
+        iCorner[d] = lower_trunc(X[d] - (d != f ? (value_type)0.5 : (value_type)0)) + coord_offset;
+      iLocalPos = X - iCorner;
+      if constexpr (kt == kernel_e::linear)
+        weights = linear_bspline_weights<deriv_order>(iLocalPos);
+      else if constexpr (kt == kernel_e::quadratic)
+        weights = quadratic_bspline_weights<deriv_order>(iLocalPos);
+      else if constexpr (kt == kernel_e::cubic)
+        weights = cubic_bspline_weights<deriv_order>(iLocalPos);
+    }
+
+#if 0
+    constexpr Arena<value_type> arena() const noexcept {
+      ;
+      ;
+    }
+#endif
+
+    // weight
+    template <typename... Tn>
+    constexpr value_type weight(const std::tuple<Tn...> &loc) const noexcept {
+      return weight_impl(loc, std::index_sequence_for<Tn...>{});
+    }
+    template <typename... Tn,
+              enable_if_all<(!is_std_tuple<Tn>() && ... && (sizeof...(Tn) == dim))> = 0>
+    constexpr auto weight(Tn &&...is) const noexcept {
+      return weight(std::forward_as_tuple(FWD(is)...));
+    }
+    // weight gradient
+    template <std::size_t I, typename... Tn, auto ord = deriv_order>
+    constexpr std::enable_if_t<(ord > 0), value_type> weightGradient(
+        const std::tuple<Tn...> &loc) const noexcept {
+      return weightGradient_impl<I>(loc, std::index_sequence_for<Tn...>{});
+    }
+    void printWeights() {
+      value_type sum = 0;
+      for (int d = 0; d != dim; ++d) {
+        for (int w = 0; w != width; ++w) {
+          sum += get<0>(weights)(d, w);
+          fmt::print("weights({}, {}): [{}]\t", d, w, get<0>(weights)(d, w));
+          if constexpr (deriv_order > 0) fmt::print("[{}]\t", get<1>(weights)(d, w));
+          if constexpr (deriv_order > 1) fmt::print("[{}]\t", get<2>(weights)(d, w));
+          fmt::print("\n");
+        }
+      }
+      fmt::print("weight sum: {}\n", sum);
+    }
+
+    lsv_t *lsPtr{nullptr};
+    WeightScratchPad weights{};
+    TV iLocalPos{TV::zeros()};  // index-space local offset
+    TV iCorner{TV::zeros()};    // index-space global coord
+
+  protected:
+    template <typename... Tn, std::size_t... Is,
+              enable_if_all<(sizeof...(Is) == dim), (sizeof...(Tn) == dim)> = 0>
+    constexpr value_type weight_impl(const std::tuple<Tn...> &loc,
+                                     index_seq<Is...>) const noexcept {
+      value_type ret{1};
+      ((void)(ret *= get<0>(weights)(Is, std::get<Is>(loc))), ...);
+      return ret;
+    }
+    template <std::size_t I, typename... Tn, std::size_t... Is, auto ord = deriv_order,
+              enable_if_all<(sizeof...(Is) == dim), (sizeof...(Tn) == dim), (ord > 0)> = 0>
+    constexpr value_type weightGradient_impl(const std::tuple<Tn...> &loc,
+                                             index_seq<Is...>) const noexcept {
+      value_type ret{1};
+      ((void)(ret *= (I == Is ? get<1>(weights)(Is, std::get<Is>(loc))
+                              : get<0>(weights)(Is, std::get<Is>(loc)))),
+       ...);
+      return ret;
+    }
+    template <typename... Tn, std::size_t... Is, auto ord = deriv_order,
+              enable_if_all<(sizeof...(Is) == dim), (sizeof...(Tn) == dim), (ord > 0)> = 0>
+    constexpr TV weightGradients_impl(const std::tuple<Tn...> &loc,
+                                      index_seq<Is...>) const noexcept {
+      return TV{weightGradient_impl<Is>(loc, index_seq<Is...>{})...};
+    }
+  };
 
   template <typename ExecPol, int dim, grid_e category>
   void flood_fill_levelset(ExecPol &&policy, SparseLevelSet<dim, category> &ls);

d421a78b4f06ea3301b91f41a6b0630bdb9ebb62
littlemine <wxlwxl1993@zju.edu.cn>
2022-06-29 08:57:26 +0800

    compile time rng

---
+++ include/zensim/math/probability/Random.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "zensim/TypeAlias.hpp"
+
+/// ref: C++ Weekly With Jason Turner - Episode 44 : constexpr Compile Time Random
+namespace zs {
+
+  constexpr u64 seed() noexcept {
+    u64 shifted = 0;
+    for (const auto c : __TIME__) {
+      shifted <<= 8;
+      shifted |= c;
+    }
+    return shifted;
+  }
+
+  struct PCG {
+    struct pcg32_random_t {
+      u64 state = 0;
+      u64 inc = seed();
+    } rng{};
+
+    using result_type = u32;
+
+    constexpr result_type pcg32_random_r() noexcept {
+      u64 oldstate = rng.state;
+      rng.state = oldstate * 6364136223846793005ULL + (rng.inc | 1);
+      u32 xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+      u32 rot = oldstate >> 59u;
+      return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+    }
+    // TODO: https://en.wikipedia.org/wiki/Linear_congruential_generator
+
+    constexpr result_type operator()() noexcept { return pcg32_random_r(); }
+  };
+
+  constexpr auto call_random(int i = 0) noexcept {
+    PCG pcg{};
+    while (i > 0) {
+      pcg();
+      --i;
+    }
+    return pcg();
+  }
+
+}  // namespace zs

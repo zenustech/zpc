0a712f51dee57e0d7b0d9cb48fa229c898ae659f
littlemine <wxlwxl1993@zju.edu.cn>
2022-11-26 12:37:52 +0800

    add smallstring ofb check and concatenation

--- include/zensim/types/SmallVector.hpp
+++ include/zensim/types/SmallVector.hpp
@@ -21,11 +21,22 @@ namespace zs {
       size_type i = 0;
       for (; i + (size_type)1 != nbytes && tmp[i]; ++i) buf[i] = tmp[i];
       buf[i] = '\0';
+#if ZS_ENABLE_OFB_ACCESS_CHECK
+      if (i == nbytes - 1 && tmp[i]) {
+        printf("the str [%s]\' size exceeds smallstring maximum length [%d]!\n", tmp, (int)nbytes);
+      }
+#endif
     }
     SmallString(const std::string &str) noexcept {
       size_type n = str.size() < nbytes ? str.size() : nbytes - 1;
       buf[n] = '\0';
       for (size_type i = 0; i != n; ++i) buf[i] = str[i];
+#if ZS_ENABLE_OFB_ACCESS_CHECK
+      if (str.size() >= nbytes) {
+        printf("the str [%s]\' size exceeds smallstring maximum length [%d]!\n", str.c_str(),
+               (int)nbytes);
+      }
+#endif
     }
     constexpr SmallString(const SmallString &) noexcept = default;
     constexpr SmallString &operator=(const SmallString &) noexcept = default;
@@ -55,6 +66,20 @@ namespace zs {
       for (; buf[i]; ++i)
         ;
       return i;
+    }
+    friend constexpr SmallString operator+(const SmallString &a, const SmallString &b) noexcept {
+      SmallString ret{};
+      size_type i = 0;
+      for (; i + (size_type)1 != nbytes && a.buf[i]; ++i) ret.buf[i] = a.buf[i];
+      for (size_type j = 0; i + (size_type)1 != nbytes && b.buf[j]; ++i, ++j) ret.buf[i] = a.buf[j];
+      ret.buf[i] = '\0';
+#if ZS_ENABLE_OFB_ACCESS_CHECK
+      if (a.size() + b.size() >= nbytes) {
+        printf("concatenating str [%s] and str [%s] exceeds smallstring maximum length [%d]!\n",
+               a.asChars(), b.asChars(), (int)nbytes);
+      }
+#endif
+      return ret;
     }
 
     char_type buf[nbytes];

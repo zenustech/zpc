3e75fdd0f2bc859d9c088d4ae614f5cc000fc327
littlemine <wxlwxl1993@zju.edu.cn>
2023-03-30 20:00:13 +0800

    avoid 'color' race condition

--- include/zensim/graph/Coloring.hpp
+++ include/zensim/graph/Coloring.hpp
@@ -28,38 +28,42 @@ namespace zs {
     policy(colors, [] ZS_LAMBDA(ColorT & color) { color = limits<ColorT>::max(); });
 
     zs::Vector<int, AllocatorT> done{spmat.get_allocator(), 2};
+    zs::Vector<u8, AllocatorT> maskOut{spmat.get_allocator(), n};
+    maskOut.reset(0);
     std::vector<int> hdone(2);
     ColorT color;
     for (color = 0;; color += 2) {
       done.reset(0);
-      policy(range(n),
-             [spmat = proxy<space>(spmat), ws = std::begin(weights), colors = std::begin(colors),
-              done = view<space>(done), color] ZS_LAMBDA(Ti row) mutable {
-               // if already colored, exit
-               if (colors[row] != limits<ColorT>::max()) return;
+      policy(range(n), [spmat = proxy<space>(spmat), ws = std::begin(weights),
+                        colors = std::begin(colors), done = view<space>(done),
+                        maskOut = view<space>(maskOut), color] ZS_LAMBDA(Ti row) mutable {
+        // if already colored, exit
+        if (maskOut[row]) return;
 
-               auto w = ws[row];
-               auto bg = spmat._ptrs[row];
-               auto ed = spmat._ptrs[row + 1];
-               bool colorMax = true;
-               bool colorMin = true;
-               for (auto k = bg; k != ed; ++k) {
-                 auto neighbor = spmat._inds[k];
-                 // skip the already colored neighbor nodes
-                 if (colors[neighbor] == limits<ColorT>::max()) {
-                   auto ow = ws[neighbor];
-                   if (ow > w) colorMax = false;
-                   if (ow < w) colorMin = false;
-                 }
-               }
-               if (colorMax) {
-                 colors[row] = color + 1;
-                 done[0] = 1;
-               } else if (colorMin) {
-                 colors[row] = color + 2;
-                 done[1] = 1;
-               }
-             });
+        auto w = ws[row];
+        auto bg = spmat._ptrs[row];
+        auto ed = spmat._ptrs[row + 1];
+        bool colorMax = true;
+        bool colorMin = true;
+        for (auto k = bg; k != ed; ++k) {
+          auto neighbor = spmat._inds[k];
+          // skip the already colored neighbor nodes
+          if (!maskOut[neighbor]) {
+            auto ow = ws[neighbor];
+            if (ow > w) colorMax = false;
+            if (ow < w) colorMin = false;
+          }
+        }
+        if (colorMax) {
+          colors[row] = color + 1;
+          maskOut[row] = 1;
+          done[0] = 1;
+        } else if (colorMin) {
+          colors[row] = color + 2;
+          maskOut[row] = 1;
+          done[1] = 1;
+        }
+      });
       done.retrieveVals(hdone.data());
       if (hdone[0] == 0) {
         break;

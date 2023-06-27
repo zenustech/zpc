9762622f639429b335ff6a402aa2fd462465a225
littlemine <wxlwxl1993@zju.edu.cn>
2023-06-27 15:41:25 +0800

    windows-friendly convention...

--- include/zensim/py_interop/TileVectorInstantiations.cpp
+++ include/zensim/py_interop/TileVectorInstantiations.cpp
@@ -3,45 +3,47 @@
 
 extern "C" {
 
-#define INSTANTIATE_TILE_VECTOR_VIEWLITE(T, L)                                                    \
-  zs::TileVectorViewLite<T, L> pyview##T##L(zs::TileVector<T, L, zs::ZSPmrAllocator<false>> &v) { \
-    return zs::TileVectorViewLite<T, L>{v.data(), v.numChannels()};                               \
-  }                                                                                               \
-  zs::TileVectorViewLite<const T, L> pyview##T##L##const(                                         \
-      const zs::TileVector<T, L, zs::ZSPmrAllocator<false>> &v) {                                 \
-    return zs::TileVectorViewLite<const T, L>{v.data(), v.numChannels()};                         \
-  }                                                                                               \
-  zs::TileVectorViewLite<T, L> pyview##T##L##virtual(                                             \
-      zs::TileVector<T, L, zs::ZSPmrAllocator<true>> & v) {                                       \
-    return zs::TileVectorViewLite<T, L>{v.data(), v.numChannels()};                               \
-  }                                                                                               \
-  zs::TileVectorViewLite<const T, L> pyview##T##L##const##virtual(                                \
-      const zs::TileVector<T, L, zs::ZSPmrAllocator<true>> &v) {                                  \
-    return zs::TileVectorViewLite<const T, L>{v.data(), v.numChannels()};                         \
-  }                                                                                               \
-  zs::TileVectorNamedViewLite<T, L> pyview##T##L##name(                                           \
-      zs::true_type, zs::TileVector<T, L, zs::ZSPmrAllocator<false>> &v) {                            \
-    return zs::TileVectorNamedViewLite<T, L>{v.data(),          v.numChannels(),                  \
-                                             v.tagNameHandle(), v.tagOffsetHandle(),              \
-                                             v.tagSizeHandle(), v.numProperties()};               \
-  }                                                                                               \
-  zs::TileVectorNamedViewLite<const T, L> pyview##T##L##name##const(                              \
-      zs::true_type, const zs::TileVector<T, L, zs::ZSPmrAllocator<false>> &v) {                      \
-    return zs::TileVectorNamedViewLite<const T, L>{v.data(),          v.numChannels(),            \
-                                                   v.tagNameHandle(), v.tagOffsetHandle(),        \
-                                                   v.tagSizeHandle(), v.numProperties()};         \
-  }                                                                                               \
-  zs::TileVectorNamedViewLite<T, L> pyview##T##L##name##virtual(                                  \
-      zs::true_type, zs::TileVector<T, L, zs::ZSPmrAllocator<true>> & v) {                            \
-    return zs::TileVectorNamedViewLite<T, L>{v.data(),          v.numChannels(),                  \
-                                             v.tagNameHandle(), v.tagOffsetHandle(),              \
-                                             v.tagSizeHandle(), v.numProperties()};               \
-  }                                                                                               \
-  zs::TileVectorNamedViewLite<const T, L> pyview##T##L##name##const##virtual(                     \
-      zs::true_type, const zs::TileVector<T, L, zs::ZSPmrAllocator<true>> &v) {                       \
-    return zs::TileVectorNamedViewLite<const T, L>{v.data(),          v.numChannels(),            \
-                                                   v.tagNameHandle(), v.tagOffsetHandle(),        \
-                                                   v.tagSizeHandle(), v.numProperties()};         \
+#define INSTANTIATE_TILE_VECTOR_VIEWLITE(T, L)                                                 \
+  zs::TileVectorViewLite<T, L> *pyview##__##tv##_##T##_##L(                                    \
+      zs::TileVector<T, L, zs::ZSPmrAllocator<false>> &v) {                                    \
+    return new zs::TileVectorViewLite<T, L>{v.data(), v.numChannels()};                        \
+  }                                                                                            \
+  zs::TileVectorViewLite<const T, L> *pyview##__##tv##_##const##_##T##_##L(                    \
+      const zs::TileVector<T, L, zs::ZSPmrAllocator<false>> &v) {                              \
+    return new zs::TileVectorViewLite<const T, L>{v.data(), v.numChannels()};                  \
+  }                                                                                            \
+  zs::TileVectorViewLite<T, L> *pyview##__##tv##_##T##_##L##_##virtual(                        \
+      zs::TileVector<T, L, zs::ZSPmrAllocator<true>> & v) {                                    \
+    return new zs::TileVectorViewLite<T, L>{v.data(), v.numChannels()};                        \
+  }                                                                                            \
+  zs::TileVectorViewLite<const T, L> *pyview##__##tv##_##const##_##T##_##L##_##virtual(        \
+      const zs::TileVector<T, L, zs::ZSPmrAllocator<true>> &v) {                               \
+    return new zs::TileVectorViewLite<const T, L>{v.data(), v.numChannels()};                  \
+  }                                                                                            \
+  zs::TileVectorNamedViewLite<T, L> *pyview##__##tv##_##named##_##named##_##T##_##L(           \
+      zs::true_type, zs::TileVector<T, L, zs::ZSPmrAllocator<false>> &v) {                     \
+    return new zs::TileVectorNamedViewLite<T, L>{v.data(),          v.numChannels(),           \
+                                                 v.tagNameHandle(), v.tagOffsetHandle(),       \
+                                                 v.tagSizeHandle(), v.numProperties()};        \
+  }                                                                                            \
+  zs::TileVectorNamedViewLite<const T, L> *pyview##__##tv##_##named##_##const##_##T##_##L(     \
+      zs::true_type, const zs::TileVector<T, L, zs::ZSPmrAllocator<false>> &v) {               \
+    return new zs::TileVectorNamedViewLite<const T, L>{v.data(),          v.numChannels(),     \
+                                                       v.tagNameHandle(), v.tagOffsetHandle(), \
+                                                       v.tagSizeHandle(), v.numProperties()};  \
+  }                                                                                            \
+  zs::TileVectorNamedViewLite<T, L> *pyview##__##tv##_##named##_##T##_##L##_##virtual(         \
+      zs::true_type, zs::TileVector<T, L, zs::ZSPmrAllocator<true>> & v) {                     \
+    return new zs::TileVectorNamedViewLite<T, L>{v.data(),          v.numChannels(),           \
+                                                 v.tagNameHandle(), v.tagOffsetHandle(),       \
+                                                 v.tagSizeHandle(), v.numProperties()};        \
+  }                                                                                            \
+  zs::TileVectorNamedViewLite<const T, L>                                                      \
+      *pyview##__##tv##_##named##_##const##_##T##_##L##_##virtual(                             \
+          zs::true_type, const zs::TileVector<T, L, zs::ZSPmrAllocator<true>> &v) {            \
+    return new zs::TileVectorNamedViewLite<const T, L>{v.data(),          v.numChannels(),     \
+                                                       v.tagNameHandle(), v.tagOffsetHandle(), \
+                                                       v.tagSizeHandle(), v.numProperties()};  \
   }
 
 INSTANTIATE_TILE_VECTOR_VIEWLITE(int, 8);
@@ -56,5 +58,4 @@ INSTANTIATE_TILE_VECTOR_VIEWLITE(double, 8);
 INSTANTIATE_TILE_VECTOR_VIEWLITE(double, 32);
 INSTANTIATE_TILE_VECTOR_VIEWLITE(double, 64);
 INSTANTIATE_TILE_VECTOR_VIEWLITE(double, 512);
-
-}  // namespace zs
+}

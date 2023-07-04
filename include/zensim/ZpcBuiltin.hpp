cf5088dbdeb411429affca7140de225cdd73551c
littlemine <wxlwxl1993@zju.edu.cn>
2023-07-05 02:20:09 +0800

    imprv print robustness

--- include/zensim/ZpcBuiltin.hpp
+++ include/zensim/ZpcBuiltin.hpp
@@ -18,53 +18,40 @@
 // #include "zensim/types/SourceLocation.hpp"
 
 namespace zs {
-  template<class T, class=int>
-  struct printf_target; 
+  template <class T, class = int> struct printf_target;
 
-  template<class T>
-  struct printf_target<T, enable_if_t<is_floating_point_v<T>>>
-  {
-    using type = float; 
-    constexpr static char placeholder[] = "%f"; 
-  }; 
+  template <class T> struct printf_target<T, enable_if_t<is_floating_point_v<T>>> {
+    using type = float;
+    constexpr static char placeholder[] = "%f";
+  };
 
-  template<class T>
-  struct printf_target<T, enable_if_t<is_integral_v<T>>>
-  {
-    using type = int; 
-    constexpr static char placeholder[] = "%d"; 
-  }; 
+  template <class T> struct printf_target<T, enable_if_t<is_integral_v<T>>> {
+    using type = int;
+    constexpr static char placeholder[] = "%d";
+  };
 
-  template<class T>
-  struct printf_target<T, enable_if_t<is_same_v<decay_t<T>, char*>>>
-  {
-    using type = const char *; 
-    constexpr static char placeholder[] = "%s"; 
-  }; 
+  template <class T> struct printf_target<
+      T, enable_if_t<is_same_v<decay_t<T>, char*> || is_same_v<decay_t<T>, const char*>>> {
+    using type = const char*;
+    constexpr static char placeholder[] = "%s";
+  };
 
-  ZS_FUNCTION SmallString join(const SmallString& joinStr)
-  {
-    return SmallString{}; 
+  ZS_FUNCTION SmallString join(const SmallString& joinStr) { return SmallString{}; }
+
+  template <class T> ZS_FUNCTION SmallString join(const SmallString& joinStr, T&& s0) { return s0; }
+
+  template <class T, class... Types>
+  ZS_FUNCTION SmallString join(const SmallString& joinStr, T&& s0, Types&&... args) {
+    return s0 + joinStr + join(joinStr, FWD(args)...);
   }
 
-  template<class T>
-  ZS_FUNCTION SmallString join(const SmallString& joinStr, T&& s0)
-  {
-    return s0; 
+  template <class... Types> ZS_FUNCTION void print_internal(Types&&... args) {
+    auto formatStr = join(" ", SmallString{printf_target<remove_cvref_t<Types>>::placeholder}...);
+    printf(formatStr.asChars(),
+           static_cast<typename printf_target<remove_cvref_t<Types>>::type>(args)...);
   }
 
-  template<class T, class... Types>
-  ZS_FUNCTION SmallString join(const SmallString& joinStr, T&& s0, Types&& ...args)
-  {
-    return s0 + joinStr + join(joinStr, FWD(args)...); 
+  template <class... Types> ZS_FUNCTION void print(Types&&... args) {
+    print_internal(FWD(args)..., "\n");
   }
-
-  template <class... Types> ZS_FUNCTION void print_internal(Types&& ...args) {
-    auto formatStr = join(" ", SmallString{printf_target<remove_cvref_t<Types>>::placeholder}...); 
-    printf(formatStr.asChars(), static_cast<typename printf_target<remove_cvref_t<Types>>::type>(args)...); 
-  }
-
-  template <class... Types> ZS_FUNCTION void print(Types &&...args) { 
-    print_internal(FWD(args)..., "\n"); 
-  }
-}
+}  // namespace zs

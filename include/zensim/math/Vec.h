dcf0d8e6cfde1097cb5b52a873844687dd642d6f
littlemine <wxlwxl1993@zju.edu.cn>
2023-08-20 06:50:33 +0800

    extending zs::vec ability

--- include/zensim/math/Vec.h
+++ include/zensim/math/Vec.h
@@ -27,9 +27,10 @@ namespace zs {
   /// vec without lifetime managing
   template <typename T, typename Tn, Tn... Ns> struct vec_view<T, integer_sequence<Tn, Ns...>>
       : VecInterface<vec_view<T, integer_sequence<Tn, Ns...>>> {
+    static constexpr bool is_pointer_structure = is_pointer_v<T>;
     using base_t = VecInterface<vec_view<T, integer_sequence<Tn, Ns...>>>;
     // essential defs for any VecInterface
-    using value_type = remove_const_t<T>;
+    using value_type = remove_pointer_t<T>;
     using index_type = Tn;
     using indexer_type = indexer<index_type, Ns...>;
     using extents = integer_sequence<index_type, Ns...>;
@@ -41,11 +42,12 @@ namespace zs {
     constexpr vec_view() = delete;
     constexpr vec_view(const vec_view &) = delete;             // prevents accidental copy of view
     constexpr vec_view &operator=(const vec_view &) = delete;  // prevents accidental copy of view
+    template <bool IsPtrStruct = is_pointer_structure, enable_if_t<!IsPtrStruct> = 0>
     constexpr explicit vec_view(T *ptr) : _data{ptr} {}
 
     constexpr explicit operator variant_vec<value_type, extents>() const noexcept {
       variant_vec<value_type, extents> r{};
-      for (index_type i = 0; i != extent; ++i) r.val(i) = this->val(i);
+      for (index_type i = 0; i != extent; ++i) r.val(i) = base_t::val(i);
       return r;
     }
 
@@ -53,38 +55,70 @@ namespace zs {
     // ()
     template <typename... Args, enable_if_t<sizeof...(Args) <= dim> = 0>
     constexpr T &operator()(Args &&...args) noexcept {
-      return _data[indexer_type::offset(forward<Args>(args)...)];
+      return base_t::val(indexer_type::offset(forward<Args>(args)...));
     }
     template <typename... Args, enable_if_t<sizeof...(Args) <= dim> = 0>
     constexpr const T &operator()(Args &&...args) const noexcept {
-      return _data[indexer_type::offset(forward<Args>(args)...)];
+      return base_t::val(indexer_type::offset(forward<Args>(args)...));
     }
     // []
     template <typename Index, enable_if_t<is_integral_v<Index>> = 0>
     constexpr decltype(auto) operator[](Index index) noexcept {
       if constexpr (dim == 1) {
-        return _data[index];
+        return base_t::val(index);
       } else {
         using R = vec_view<T, gather_t<typename gen_seq<dim - 1>::template arithmetic<1>, extents>>;
-        return R{_data + indexer_type::offset(index)};
+        auto offset = indexer_type::offset(index);
+        if constexpr (is_pointer_structure) {
+          R ret{};
+          for (index_type i = 0; i != R::extent; ++i) ret.data()[i] = base_t::data()[offset + i];
+          return ret;
+        } else
+          return R{base_t::data() + offset};
       }
     }
     template <typename Index, enable_if_t<is_integral_v<Index>> = 0>
     constexpr decltype(auto) operator[](Index index) const noexcept {
       if constexpr (dim == 1) {
-        return _data[index];
+        return base_t::val(index);
       } else {
         using R = vec_view<add_const_t<T>,
                            gather_t<typename gen_seq<dim - 1>::template arithmetic<1>, extents>>;
-        return R{_data + indexer_type::offset(index)};
+        auto offset = indexer_type::offset(index);
+        if constexpr (is_pointer_structure) {
+          R ret{};
+          for (index_type i = 0; i != R::extent; ++i) ret.data()[i] = base_t::data()[offset + i];
+          return ret;
+        } else
+          return R{base_t::data() + offset};
       }
     }
-    template <typename Index> constexpr T &do_val(Index index) noexcept { return _data[index]; }
-    template <typename Index> constexpr const T &do_val(Index index) const noexcept {
-      return _data[index];
+    template <typename Index> constexpr decltype(auto) do_val(Index index) noexcept {
+      if constexpr (is_pointer_structure)
+        return *_data[index];
+      else
+        return _data[index];
+    }
+    template <typename Index> constexpr decltype(auto) do_val(Index index) const noexcept {
+      if constexpr (is_pointer_structure)
+        return *_data[index];
+      else
+        return _data[index];
+    }
+    template <typename Index> constexpr decltype(auto) do_val(Index index) volatile noexcept {
+      if constexpr (is_pointer_structure)
+        return *_data[index];
+      else
+        return _data[index];
+    }
+    template <typename Index> constexpr decltype(auto) do_val(Index index) const volatile noexcept {
+      if constexpr (is_pointer_structure)
+        return *_data[index];
+      else
+        return _data[index];
     }
 
-    T *_data;
+    conditional_t<is_pointer_structure, T[extent], T *> _data;
   };
 
   /// vec
@@ -92,8 +126,9 @@ namespace zs {
       : VecInterface<vec_impl<T, integer_sequence<Tn, Ns...>>> {
     // static_assert(std::is_trivial<T>::value,
     //              "Vec element type is not trivial!\n");
+    static constexpr bool is_pointer_structure = is_pointer_v<T>;
     using base_t = VecInterface<vec_impl<T, integer_sequence<Tn, Ns...>>>;
-    using value_type = T;
+    using value_type = remove_pointer_t<T>;
     using index_type = Tn;
     using indexer_type = indexer<index_type, Ns...>;
     using extents = integer_sequence<index_type, Ns...>;
@@ -102,18 +137,18 @@ namespace zs {
 
     template <typename OtherT, typename IndicesT> using variant_vec = vec_impl<OtherT, IndicesT>;
 
-    T _data[extent];
+    T _data[extent];  // empty-initialized for both arithmetic types and pointer types
 
   public:
     /// expose internal
-    constexpr auto data() noexcept -> T * { return _data; }
-    constexpr auto data() const volatile noexcept -> const volatile T * {
+    constexpr auto do_data() noexcept -> T * { return _data; }
+    constexpr auto do_data() const volatile noexcept -> const volatile T * {
       return const_cast<const volatile T *>(_data);
     }
-    constexpr auto data() volatile noexcept -> volatile T * {
+    constexpr auto do_data() volatile noexcept -> volatile T * {
       return const_cast<volatile T *>(_data);
     }
-    constexpr auto data() const noexcept -> const T * { return const_cast<const T *>(_data); }
+    constexpr auto do_data() const noexcept -> const T * { return const_cast<const T *>(_data); }
 
     /// think this does not break rule of five
     constexpr vec_impl() noexcept = default;
@@ -136,7 +171,7 @@ namespace zs {
               enable_if_t<sizeof...(Args) == extent> = 0>
     static constexpr vec_impl from_tuple(const TupT<Args...> &tup, index_sequence<Is...>) {
       vec_impl ret{};
-      ((void)(ret.data()[Is] = get<Is>(tup)), ...);  // ADL
+      ((void)(base_t::val(Is) = get<Is>(tup)), ...);  // ADL
       return ret;
     }
     template <template <typename...> class TupT, typename... Args,
@@ -165,42 +200,50 @@ namespace zs {
     template <template <typename, zs::size_t> class ArrayT = std::array>
     constexpr ArrayT<T, extent> to_array() const noexcept {
       ArrayT<T, extent> r{};
-      for (Tn i = 0; i != extent; ++i) r[i] = _data[i];
+      for (Tn i = 0; i != extent; ++i) r[i] = base_t::val(i);
       return r;
     }
     /// random access
     // ()
     template <typename... Args, enable_if_t<sizeof...(Args) <= dim> = 0>
     constexpr T &operator()(Args &&...args) noexcept {
-      return _data[indexer_type::offset(forward<Args>(args)...)];
+      return base_t::val(indexer_type::offset(forward<Args>(args)...));
     }
     template <typename... Args, enable_if_t<sizeof...(Args) <= dim> = 0>
     constexpr const T &operator()(Args &&...args) const noexcept {
-      return _data[indexer_type::offset(forward<Args>(args)...)];
+      return base_t::val(indexer_type::offset(forward<Args>(args)...));
     }
     // []
-    template <typename Index,
-              typename R
-              = vec_view<T, gather_t<typename gen_seq<dim - 1>::template arithmetic<1>, extents>>,
-              Tn d = dim, enable_if_t<(d > 1)> = 0>
-    constexpr R operator[](Index index) noexcept {
-      return R{_data + indexer_type::offset(index)};
+    template <typename Index, enable_if_t<is_integral_v<Index>> = 0>
+    constexpr decltype(auto) operator[](Index index) noexcept {
+      if constexpr (dim == 1) {
+        return base_t::val(index);
+      } else {
+        using R = vec_view<T, gather_t<typename gen_seq<dim - 1>::template arithmetic<1>, extents>>;
+        auto offset = indexer_type::offset(index);
+        if constexpr (is_pointer_structure) {
+          R ret{};
+          for (index_type i = 0; i != R::extent; ++i) ret.data()[i] = base_t::data()[offset + i];
+          return ret;
+        } else
+          return R{base_t::data() + offset};
+      }
     }
-    template <typename Index,
-              typename R
-              = vec_view<add_const_t<T>,
-                         gather_t<typename gen_seq<dim - 1>::template arithmetic<1>, extents>>,
-              Tn d = dim, enable_if_t<(d > 1)> = 0>
-    constexpr R operator[](Index index) const noexcept {
-      return R{_data + indexer_type::offset(index)};
-    }
-    template <typename Index, Tn d = dim, enable_if_t<d == 1> = 0>
-    constexpr T &operator[](Index index) noexcept {
-      return _data[index];
-    }
-    template <typename Index, Tn d = dim, enable_if_t<d == 1> = 0>
-    constexpr const T &operator[](Index index) const noexcept {
-      return _data[index];
+    template <typename Index, enable_if_t<is_integral_v<Index>> = 0>
+    constexpr decltype(auto) operator[](Index index) const noexcept {
+      if constexpr (dim == 1) {
+        return base_t::val(index);
+      } else {
+        using R = vec_view<add_const_t<T>,
+                           gather_t<typename gen_seq<dim - 1>::template arithmetic<1>, extents>>;
+        auto offset = indexer_type::offset(index);
+        if constexpr (is_pointer_structure) {
+          R ret{};
+          for (index_type i = 0; i != R::extent; ++i) ret.data()[i] = base_t::data()[offset + i];
+          return ret;
+        } else
+          return R{base_t::data() + offset};
+      }
     }
     template <typename TT, enable_if_all<(alignof(value_type) >= alignof(TT)),
                                          sizeof(value_type) == sizeof(TT)> = 0>
@@ -213,14 +256,34 @@ namespace zs {
       return base_t::reinterpret_bits(wrapt<TT>{});
 #endif
     }
-    template <typename Index> constexpr T &do_val(Index index) noexcept { return _data[index]; }
-    template <typename Index> constexpr const T &do_val(Index index) const noexcept {
-      return _data[index];
+    template <typename Index> constexpr decltype(auto) do_val(Index index) noexcept {
+      if constexpr (is_pointer_structure)
+        return *_data[index];
+      else
+        return _data[index];
+    }
+    template <typename Index> constexpr decltype(auto) do_val(Index index) const noexcept {
+      if constexpr (is_pointer_structure)
+        return *_data[index];
+      else
+        return _data[index];
+    }
+    template <typename Index> constexpr decltype(auto) do_val(Index index) volatile noexcept {
+      if constexpr (is_pointer_structure)
+        return *_data[index];
+      else
+        return _data[index];
+    }
+    template <typename Index> constexpr decltype(auto) do_val(Index index) const volatile noexcept {
+      if constexpr (is_pointer_structure)
+        return *_data[index];
+      else
+        return _data[index];
     }
     ///
     template <typename TT> constexpr explicit operator vec_impl<TT, extents>() const noexcept {
       vec_impl<TT, extents> r{};
-      for (Tn idx = 0; idx != extent; ++idx) r.val(idx) = _data[idx];
+      for (Tn idx = 0; idx != extent; ++idx) r.val(idx) = val(idx);
       return r;
     }
   };

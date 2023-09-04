683233edbf2acfa3fdc67edbf15dc427fdcf82ef
littlemine <wxlwxl1993@zju.edu.cn>
2023-09-05 04:16:33 +0800

    minor

--- include/zensim/ImplPattern.hpp
+++ include/zensim/ImplPattern.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "zensim/ZpcMeta.hpp"
+#include "zensim/ZpcReflection.hpp"
 
 namespace zs {
 
@@ -7,21 +8,37 @@ namespace zs {
       : private Skills<Object<Skills...>>... {};
   template <typename Derived, template <typename> class... Skills> struct
 #if defined(ZS_COMPILER_MSVC)
-      ///  ref: https://stackoverflow.com/questions/12701469/why-is-the-empty-base-class-optimization-ebo-is-not-working-in-msvc
-      ///  ref: https://devblogs.microsoft.com/cppblog/optimizing-the-layout-of-empty-base-classes-in-vs2015-update-2-3/
+      ///  ref:
+      ///  https://stackoverflow.com/questions/12701469/why-is-the-empty-base-class-optimization-ebo-is-not-working-in-msvc
+      ///  ref:
+      ///  https://devblogs.microsoft.com/cppblog/optimizing-the-layout-of-empty-base-classes-in-vs2015-update-2-3/
       __declspec(empty_bases)
 #endif
           Mixin : public Skills<Derived>... {
   };
 
-  template <typename Derived> struct Visitee {
-    constexpr auto derivedPtr() noexcept { return static_cast<Derived*>(this); }
-    constexpr auto derivedPtr() const noexcept { return static_cast<const Derived*>(this); }
-    constexpr auto derivedPtr() volatile noexcept { return static_cast<volatile Derived*>(this); }
-    constexpr auto derivedPtr() const volatile noexcept {
-      return static_cast<const volatile Derived*>(this);
-    }
+  struct ObjectConcept {
+    virtual ~ObjectConcept() = default;
+  };
+  // struct NodeConcept {
+  // NodeConcept(NodeConcept* parent) noexcept : _parent{parent} {}
+  // NodeConcept* _parent{nullptr};
+  // };
 
+#define ZS_SUPPLEMENT_IMPL_PATTERN_DERIVED_ACCESS                                                \
+  constexpr auto derivedPtr() noexcept { return static_cast<Derived*>(this); }                   \
+  constexpr auto derivedPtr() const noexcept { return static_cast<const Derived*>(this); }       \
+  constexpr auto derivedPtr() volatile noexcept { return static_cast<volatile Derived*>(this); } \
+  constexpr auto derivedPtr() const volatile noexcept {                                          \
+    return static_cast<const volatile Derived*>(this);                                           \
+  }
+
+  template <typename Derived> struct Tagged {
+    constexpr auto typeName() const { return get_type_str<Derived>(); }
+  };
+
+  template <typename Derived> struct Visitee {
+    ZS_SUPPLEMENT_IMPL_PATTERN_DERIVED_ACCESS
     constexpr void accept(...) {}
     constexpr void accept(...) const {}
 
@@ -43,6 +60,19 @@ namespace zs {
         -> decltype(FWD(visitor)(FWD(pol), declval<const Derived&>())) {
       return FWD(visitor)(FWD(pol), *derivedPtr());
     }
+  };
+
+  template <typename Derived> struct Observable {
+    ZS_SUPPLEMENT_IMPL_PATTERN_DERIVED_ACCESS
+    // constexpr void subscribe(...) {}
+    // constexpr void unsubscribe(...) {}
+  };
+
+  template <typename Derived> struct Observer {
+    ZS_SUPPLEMENT_IMPL_PATTERN_DERIVED_ACCESS
+    // constexpr void onNext(...) {}
+    // constexpr void onComplete(...) {}
+    // constexpr void onError(...) {}
   };
 
 }  // namespace zs

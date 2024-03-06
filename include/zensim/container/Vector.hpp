fb2f7b4e5e5055cbc720f9fe24ccda1afad31153
littlemine <wxlwxl1993@zju.edu.cn>
2024-03-06 12:33:09 +0800

    minor upd

--- include/zensim/container/Vector.hpp
+++ include/zensim/container/Vector.hpp
@@ -337,6 +337,9 @@ namespace zs {
     template <typename Policy, typename MapRange, bool Scatter = false>
     void reorder(Policy &&pol, MapRange &&mapR, wrapv<Scatter> = {});
 
+    size_type &refSize() { return _size; }
+    size_type &refCapacity() { return _capacity; }
+
   protected:
     constexpr size_t usedBytes() const noexcept { return sizeof(T) * size(); }
 
@@ -621,7 +624,14 @@ namespace bitsery {
 
 #  if ZS_ENABLE_SERIALIZATION
 template <typename S, typename T> void serialize(S &s, zs::Vector<T, zs::ZSPmrAllocator<>> &c) {
-  s.template container<sizeof(T)>(c, zs::detail::deduce_numeric_max<size_t>());
+  using C = zs::Vector<T, zs::ZSPmrAllocator<>>;
+  if (c.memoryLocation().onHost()) {
+    c = c.clone({zs::memsrc_e::host, -1});
+  }
+  s.template container<sizeof(typename C::value_type)>(
+      c, zs::detail::deduce_numeric_max<typename C::size_type>());
+  s.template value<sizeof(typename C::size_type)>(c.refSize());
+  s.template value<sizeof(typename C::size_type)>(c.refCapacity());
 }
 #  endif
 #endif
